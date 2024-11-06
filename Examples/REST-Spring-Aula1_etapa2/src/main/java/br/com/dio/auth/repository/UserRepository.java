ff4ec0c6e32b4c3c77a16889d6653a4b59545253
FrancisBFTC <bftcorporations@gmail.com>
2024-11-05 21:19:19 -0300

    feat: Estruturação de pastas para atividade de extensão

--- Examples/REST-Spring-Aula1_etapa2/src/main/java/br/com/dio/auth/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.dio.auth.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.dio.auth.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long>{
-	User findByUsername(String username);
-}
--- Examples/REST-Spring-Aula1_etapa2/src/main/java/br/com/dio/auth/service/IAuthenticationManager.java
+++
@@ -1,9 +0,0 @@
-package br.com.dio.auth.service;
-
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface IAuthenticationManager extends AuthenticationManager {
-
-}
